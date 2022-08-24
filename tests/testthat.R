05e8e54babe28c262c05bba8271553c0006e53aa
Jonas Lieth <jonas.lieth@gesis.org>
2022-08-24 13:38:39 +0200

    cleaned up directory

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,8 +4,12 @@ library(ORSRouting)
 test_check("ORSRouting")
 
 # The package isn't intended to be used on Mac or Solaris
-if (Sys.info()["sysname"] == "Darwin") return()
-if (Sys.info()["sysname"] == "SunOS") return()
+if (Sys.info()["sysname"] == "Darwin") {
+  return()
+}
+if (Sys.info()["sysname"] == "SunOS") {
+  return()
+}
 
 # The tests are supposed to be run entirely and not stop on errors.
 # If the tests stop during the process, you may risk setting up containers
