cf8cb758c52966dbc12cdb3370eb584f4fa0c1de
Jonas Lieth <jonas.lieth@gesis.org>
2022-08-26 13:08:36 +0200

    assert now takes dot arguments for custom error messages

--- R/assert.R
+++ R/assert.R
@@ -5,41 +5,42 @@ assert <- function(x,
                    nrow = NULL,
                    obj = NULL,
                    set = NULL,
+                   ...,
                    abort = TRUE) {
   checks <- list()
 
   if (!is.null(class)) {
-    checks[[1]] <- assert_class(x, class, abort)
+    checks[[1]] <- assert_class(x, class, abort, ...)
   }
 
   if (!is.null(len)) {
     if (length(len) == 1) len <- rep(len, 2)
-    checks[[2]] <- assert_length(x, min = len[1], max = len[2], abort)
+    checks[[2]] <- assert_length(x, min = len[1], max = len[2], abort, ...)
   }
 
   if (!is.null(val)) {
     if (length(val) == 1) val <- rep(val, 2)
-    checks[[3]] <- assert_value(x, min = val[1], max = val[2], abort)
+    checks[[3]] <- assert_value(x, min = val[1], max = val[2], abort, ...)
   }
 
   if (!is.null(nrow) && length(nrow) == 2) {
     if (length(nrow) == 1) nrow <- rep(nrow, 2)
-    checks[[4]] <- assert_nrow(x, min = nrow[1], nrow[2], abort)
+    checks[[4]] <- assert_nrow(x, min = nrow[1], nrow[2], abort, ...)
   }
 
   if (!is.null(obj)) {
-    checks[[5]] <- assert_identical(x, obj, abort)
+    checks[[5]] <- assert_identical(x, obj, abort, ...)
   }
 
   if (!is.null(set)) {
-    checks[[6]] <- assert_element(x, set)
+    checks[[6]] <- assert_element(x, set, ...)
   }
 
   do.call(all, checks)
 }
 
 
-assert_class <- function(x, class, abort = TRUE) {
+assert_class <- function(x, class, abort = TRUE, ...) {
   afuns <- lapply(class, function(c) {
     get0(paste0("is.", c), ifnotfound = \(obj) inherits(obj, c))
   })
@@ -60,7 +61,8 @@ assert_class <- function(x, class, abort = TRUE) {
 
     cli::cli_abort(c(
       "x" = "{.var {var}} is expected to be of class {.cls2 {class}}.",
-      "i" = "Got {.cls {class(x)}} instead."
+      "!" = "Got {.cls {class(x)}} instead.",
+      ...
     ))
   } else {
     TRUE
@@ -68,7 +70,7 @@ assert_class <- function(x, class, abort = TRUE) {
 }
 
 
-assert_length <- function(x, min = 0L, max = Inf, abort = TRUE) {
+assert_length <- function(x, min = 0L, max = Inf, abort = TRUE, ...) {
   if (length(x) > max || length(x) < min) {
     if (!abort) {
       return(FALSE)
@@ -77,7 +79,7 @@ assert_length <- function(x, min = 0L, max = Inf, abort = TRUE) {
     if (min == max) {
       cli::cli_abort(c(
         "x" = "{.var {var}} is expected to be of length {.val {min}}",
-        "i" = "Got object of length {.val {length(x)}} instead."
+        "!" = "Got object of length {.val {length(x)}} instead."
       ))
     } else {
       cli::cli_abort(c(
@@ -85,7 +87,8 @@ assert_length <- function(x, min = 0L, max = Inf, abort = TRUE) {
           "{.var {var}} is expected to be between length",
           "{.val {min}} and {.val {max}}."
         ),
-        "i" = "Got object of length {.val {length(x)}} instead."
+        "!" = "Got object of length {.val {length(x)}} instead.",
+        ...
       ))
     }
   } else {
@@ -94,7 +97,7 @@ assert_length <- function(x, min = 0L, max = Inf, abort = TRUE) {
 }
 
 
-assert_value <- function(x, min = -Inf, max = Inf, abort = TRUE) {
+assert_value <- function(x, min = -Inf, max = Inf, abort = TRUE, ...) {
   if (x > max || x < min) {
     if (!abort) {
       return(FALSE)
@@ -103,7 +106,7 @@ assert_value <- function(x, min = -Inf, max = Inf, abort = TRUE) {
     if (min == max) {
       cli::cli_abort(c(
         "x" = "{.var {var}} is expected to equal {.val {min}}",
-        "i" = "Got value {.val {x}} instead."
+        "!" = "Got value {.val {x}} instead."
       ))
     } else {
       cli::cli_abort(c(
@@ -111,7 +114,8 @@ assert_value <- function(x, min = -Inf, max = Inf, abort = TRUE) {
           "{.var {var}} is expected to fall",
           "between {.val {min}} and {.val {max}}"
         ),
-        "i" = "Got value {.val {x}} instead."
+        "!" = "Got value {.val {x}} instead.",
+        ...
       ))
     }
   } else {
@@ -120,7 +124,7 @@ assert_value <- function(x, min = -Inf, max = Inf, abort = TRUE) {
 }
 
 
-assert_nrow <- function(x, min = 0L, max = Inf, abort = TRUE) {
+assert_nrow <- function(x, min = 0L, max = Inf, abort = TRUE, ...) {
   if (nrow(x) > max || nrow(x) < min) {
     if (!abort) {
       return(FALSE)
@@ -129,7 +133,7 @@ assert_nrow <- function(x, min = 0L, max = Inf, abort = TRUE) {
     if (min == max) {
       cli::cli_abort(c(
         "x" = "{.var {var}} is expected to have {.val {min}} rows.",
-        "i" = "Got an object with {.val {nrow(x)}} rows instead."
+        "!" = "Got an object with {.val {nrow(x)}} rows instead."
       ))
     } else {
       cli::cli_abort(c(
@@ -137,7 +141,8 @@ assert_nrow <- function(x, min = 0L, max = Inf, abort = TRUE) {
           "{.var {var}} is expected to have between",
           "{.val {min}} and {.val {max}} rows."
         ),
-        "i" = "Got an object with {.val {nrow(x)}} rows instead."
+        "!" = "Got an object with {.val {nrow(x)}} rows instead.",
+        ...
       ))
     }
   } else {
@@ -146,7 +151,7 @@ assert_nrow <- function(x, min = 0L, max = Inf, abort = TRUE) {
 }
 
 
-assert_identical <- function(x, obj, abort = TRUE) {
+assert_identical <- function(x, obj, abort = TRUE, ...) {
   if (!identical(x, obj)) {
     if (!abort) {
       return(FALSE)
@@ -154,7 +159,8 @@ assert_identical <- function(x, obj, abort = TRUE) {
     var <- deparse(substitute(x, env = parent.frame()))
     obj_var <- deparse(substitute(obj, env = parent.frame()))
     cli::cli_abort(c(
-      "x" = "{.var {var}} is expected to be identical to {.var {obj_var}}."
+      "x" = "{.var {var}} is expected to be identical to {.var {obj_var}}.",
+      ...
     ))
   } else {
     TRUE
@@ -162,7 +168,7 @@ assert_identical <- function(x, obj, abort = TRUE) {
 }
 
 
-assert_element <- function(x, set, abort = TRUE) {
+assert_element <- function(x, set, abort = TRUE, ...) {
   if (!is.element(x, set)) {
     if (!abort) {
       return(FALSE)
@@ -170,7 +176,8 @@ assert_element <- function(x, set, abort = TRUE) {
     var <- deparse(substitute(x, env = parent.frame()))
     set_var <- deparse(substitute(set, env = parent.frame()))
     cli::cli_abort(c(
-      "x" = "{.var {var}} is expected to be an element of {.var {set_var}}"
+      "x" = "{.var {var}} is expected to be an element of {.var {set_var}}",
+      ...
     ))
   } else {
     TRUE
