363db0f1afd8b13535f5726d0e0d1c86116b3bd9
JsLth <jslth@outlook.com>
2022-08-30 01:02:02 +0200

    added a progress argument to ors_shortest_distances

--- R/route-distances.R
+++ R/route-distances.R
@@ -317,6 +317,10 @@ ors_distances <- function(source,
 #' Type of proximity that the calculations should be
 #' based on. If `distance`, the shortest physical distance will be calculated
 #' and if `duration`, the shortest temporal distance will be calculated.
+#' @param progress \code{[logical]}
+#' 
+#' If \code{TRUE}, displays a progress bar if the process is taking a bit
+#' longer.
 #'
 #' @export
 #'
@@ -334,11 +338,13 @@ ors_shortest_distances <- function(source,
                                    geometry = FALSE,
                                    instance = NULL,
                                    ...,
-                                   proximity_type = c("duration", "distance")) {
+                                   proximity_type = c("duration", "distance"),
+                                   progress = TRUE) {
   if (is.null(instance)) {
     instance <- get_instance()
   }
 
+  verbose <- progress
   proximity_type <- match.arg(proximity_type)
   source <- format_input_data(source)
   destination <- format_input_data(destination)
@@ -388,7 +394,7 @@ ors_shortest_distances <- function(source,
 
     best_route <- cbind(best_index, routes[best_index, ])
 
-    cli::cli_progress_update(.envir = parent.frame(2L))
+    ors_cli(progress = "update", .envir = parent.frame(2L))
     best_route
   }
 
@@ -399,7 +405,8 @@ ors_shortest_distances <- function(source,
     stringsAsFactors = FALSE
   )
 
-  cli::cli_progress_bar(
+  ors_cli(
+    progress = "bar",
     name = "Calculating shortest routes...",
     total = nrow(source) * length(profile),
   )
@@ -446,63 +453,5 @@ ors_shortest_distances <- function(source,
     source = source,
     dest = destination,
     class = c("ors_sdist", class(route_df))
-  )
-}
-
-
-#' Routing distance matrix
-#' @description Calls the matrix service and returns a routing distance matrix.
-#' @inheritParams ors_distances
-#' @returns If \code{length(proximity_type) == 1}, returns a
-#' \code{nrow(source) * nrow(destination)} routing distance matrix. Otherwise,
-#' returns a list containing two matrices accordingly.
-#'
-#' @export
-ors_matrix <- function(source,
-                       destination,
-                       profile = get_profiles(),
-                       units = c("m", "km", "mi"),
-                       proximity_type = c("distance", "duration"),
-                       instance = NULL) {
-  if (is.null(instance)) {
-    instance <- get_instance()
-  }
-  iid <- get_id(instance = instance)
-
-  # Check if ORS is ready to use
-  ors_ready(force = FALSE, error = TRUE, id = iid)
-
-  profile <- match.arg(profile)
-  proximity_type <- match.arg(proximity_type)
-  units <- match.arg(units)
-
-  source <- format_input_data(source)
-  destination <- format_input_data(destination)
-
-  url <- get_ors_url(id = iid)
-
-  res <- call_ors_matrix(
-    source = source,
-    destination = destination,
-    profile = profile,
-    metrics = proximity_type,
-    units = units,
-    url = url,
-    token = instance$token
-  )
-
-  handle_ors_conditions(res, abort_on_error = TRUE, warn_on_warning = TRUE)
-
-  if (length(proximity_type) == 1L) {
-    matrix <- res[[paste0(proximity_type, "s")]]
-  } else {
-    matrix <- list(distances = res$distances, durations = res$durations)
-  }
-
-  structure(
-    matrix,
-    call = match.call(),
-    data = cbind(source, destination),
-    class = c("ors_matrix", class(matrix))
   )
 }
