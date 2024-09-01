062414dd0101466cc25fe762e9a3b878c0e78092
JsLth <jslth@outlook.com>
2024-09-02 01:30:38 +0200

    copyright symbol to ascii

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste(
-    "\u00a9 openrouteservice.org by HeiGIT | Data \u00a9 OpenStreetMap",
+    "(c) openrouteservice.org by HeiGIT | Data (c) OpenStreetMap",
     "contributors, ODbL 1.0. https://www.openstreetmap.org/copyright"
   ))
 }
